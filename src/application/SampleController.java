4e69d92cf6357f2888486ece4aa6876f818a727f
mrhpn <mr.htetphyonaing@gmail.com>
2021-03-03 15:02:29 +0630

    custom font demo

---
+++ src/application/SampleController.java
@@ -0,0 +1,16 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.text.Font;
+
+public class SampleController {
+
+    @FXML
+    private Label customFont;
+	
+}
